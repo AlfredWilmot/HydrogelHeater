0db9c04c1c5a9349aee0fdf07101fce380ee6965
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2020-07-19 00:03:46 +0100

    added OLED 128x32 adafruit lib, displaying data onto display

--- Arduino_code/T_sense_OLED_disp/T_sense_OLED_disp.ino
+++ Arduino_code/T_sense_OLED_disp/T_sense_OLED_disp.ino
@@ -1,43 +1,51 @@
 #include <Wire.h>
-
-
-void testdrawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  for(int16_t i=0; i<256; i++) {
-    if(i == '\n') display.write(' ');
-    else          display.write(i);
-  }
-
-  display.display();
-  delay(2000);
-}
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-
-int sensorValue = 0;        // value read from the pot
+int sensorValue  = 0;        // value read from the pot
+int prev_val = 0;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
+  
+  // I2C preamble for OLED display.
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Preparing display
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  //display.cp437(true);         // Use full 256 char 'Code Page 437' font
 }
 
 void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
 
-
   // print the results to the Serial Monitor:
   Serial.println(sensorValue);
 
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
+  if(sensorValue != prev_val)
+  {
+    display.setCursor(0, 0);
+    display.clearDisplay();
+    display.println(sensorValue);
+    display.display();  
+  }
+
+  prev_val = sensorValue;
+   
   delay(2);
 }
